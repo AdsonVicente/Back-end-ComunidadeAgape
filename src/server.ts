6c75346e7176c338724bec8fdb0377e4bcdb8382
Adson Vicente <adsonv04@gmail.com>
2025-06-12 14:53:45 -0300

    Adicionando Crypto e corrigindo erro import

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ app.use(helmet());
 
 // Permitir apenas domínios específicos no CORS (em produção!)
 app.use(cors({
-    'origin': ['https://comagape.org', 'http://localhost:5173'],
+    'origin': ['https://www.comagape.org', 'http://localhost:5173'],
     'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
 }));
 
